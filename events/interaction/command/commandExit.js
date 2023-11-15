6e0eabc8427a4f47fc4b67e973ca9975ba6a0948
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-11-15 17:46:22 +0100

    Update commandExit.js

--- events/interaction/command/commandExit.js
+++ events/interaction/command/commandExit.js
@@ -1,23 +1,43 @@
+const { PORT, BOT_ID } = require('../../../config/secret.json');
 const { Events } = require('discord.js');
 const { logsEmiter } = require('../../../functions/logs');
+const axios = require('axios');
 
-let client;
+const commandExitLove = (client) => {
+  client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const { commandName } = interaction;
+    
+    if(commandName === 'exit') {
+      const user = interaction.user.id;
 
-const commandExitLove = (clientItem) => {
+      try {
+        const removeLover = await axios({
+          method: "delete",
+          url: "/love/exit",
+          baseURL: `http://localhost:${PORT}`,
+          headers: {
+            "botid": BOT_ID
+          },
+          data: {
+            user_id: user,
+          }
+        });
 
-    client = clientItem;
-
-    client.on(Events.InteractionCreate, async interaction => {
-        if (!interaction.isChatInputCommand()) return;
-            const { commandName } = interaction;
-        
-        if(commandName === 'exit') {
-            try {
-                
-            }
-            catch(error) { logsEmiter(`An error occured [commandExitLove] : \r\n ${error}`); }
+        if(removeLover.status === 200) {
+          interaction.reply({
+            content: `Tu n'a plus de love de programmé mais je te love quand même. ❤️`
+          });
         }
-    });
+      }
+      catch(error) {
+        logsEmiter(`An error occured [commandExitLove] : \r\n ${error}`);
+        interaction.reply({
+          content: `Une erreur est survenue il faudra réessayer plus tard.`
+        });
+      }
+    }
+  });
 }
 
 module.exports = { commandExitLove }
