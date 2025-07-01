9db40d648c0da69734b028c5446b194d0d77fe84
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-07-01 16:04:18 +0200

    remove files

--- middlewares/isBot.js
+++
@@ -1,9 +0,0 @@
-const isBot = async (req, res, next) => {
-  if (req.headers.botid === process.env.BOT_ID) {
-    next();
-  } else {
-    res.status(403).json({ message: "Not authorized" });
-  }
-};
-
-export default isBot;
--- models/Love.js
+++
@@ -1,9 +0,0 @@
-import { model } from "mongoose";
-
-const love = model("Love", {
-  hours: String,
-  minutes: String,
-  user_id: String,
-});
-
-export default love;
